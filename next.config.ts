c1f73aa8ba29b9ea2fbbe9335c240c09ed4da2d4
selvaganapathi-coder <selvaganapathispeaker@gmail.com>
2026-08-15 21:50:10 +0530

    chore: initialize OpenNext Cloudflare development integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
